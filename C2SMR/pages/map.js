373d6b947da83bd9bf8a77138d67b1e16fd9a77b
dalet <victordalet@protonmail.com>
2023-09-24 16:30:15 +0200

    fix(map): Fix init position

--- C2SMR/pages/map.js
+++ C2SMR/pages/map.js
@@ -13,36 +13,17 @@ export class Map extends React.Component {
     this.state = {
       set_page_name: set_name,
       init_position: {
-        latitude: 48.994119156081254,
-        longitude: 2.2246659661397366,
+        latitude: 0,
+        longitude: 0,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
-      pointer_position: [
-      ],
+      pointer_position: [],
     };
     this.get_data();
   }
 
   get_data() {
-    // init position
-    fetch(url_api + "/get_init_position", {
-      method: "POST",
-      body: JSON.stringify({
-        city: this.city,
-      }),
-    })
-      .then((r) => r.json())
-      .then((r) =>
-        this.setState({
-          init_position: {
-            latitude: r["latitude"],
-            longitude: r["longitude"],
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          },
-        })
-      );
     // all pointer position
     fetch(url_api + "/get_all_position", {
       method: "POST",
@@ -55,25 +36,42 @@ export class Map extends React.Component {
         this.setState({
           pointer_position: r["data"],
         });
+        // Change init Region
+        r["data"].map((array) => {
+          if (array[2] === this.city[0]) {
+            this.setState({
+              init_position: {
+                latitude: array[0],
+                longitude: array[1],
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              },
+            });
+          }
+        });
       });
   }
 
   render() {
     return (
       <View>
-        <MapView
-          initialRegion={this.state.init_position}
-          style={images_styles.actual_picture}
-        >
-          {this.state.pointer_position.map((a, i) => (
-            <Marker
-              key={"mark-" + i.toString()}
-              coordinate={{ latitude: a[0], longitude: a[1] }}
-              title={a[2]}
-              description={"nb alert : " + a[3].toString()}
-            />
-          ))}
-        </MapView>
+        {this.state.init_position.latitude !== 0 ? (
+          <MapView
+            initialRegion={this.state.init_position}
+            style={images_styles.actual_picture}
+          >
+            {this.state.pointer_position.map((a, i) => (
+              <Marker
+                key={"mark-" + i.toString()}
+                coordinate={{ latitude: a[0], longitude: a[1] }}
+                title={a[2]}
+                description={"nb alert : " + a[3].toString()}
+              />
+            ))}
+          </MapView>
+        ) : (
+          ""
+        )}
         <Nav_bar number_page={3} set_name={this.state.set_page_name} />
       </View>
     );
